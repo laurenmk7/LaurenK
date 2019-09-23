3331ad45df640c8fa34ff1da37afe3ce108111e3
Lauren Kelly <lkelly785@gmail.com>
2019-09-23 16:11:02 -0400

    commit new folders and files to backup GitHub

---
+++ week4/var.js
@@ -0,0 +1,24 @@
+/* Strings */
+let firstName = "Lauren";
+let lastName = "Kelly";
+let phoneNumber = "617-543-9216";
+console.log(firstName);
+console.log(lastName);
+console.log(phoneNumber);
+
+
+
+/* Numbers */
+let rent = 1230;
+let phoneBill = 30;
+let internet = 45;
+alert( rent);
+alert( phoneBill);
+alert( internet);
+
+
+/* Booleans */
+let passTheClass = true; // a true value means in this case that LK does pass the class
+let passWK4ClassLab = false; // a false value means that LK does not pass class lab for week4
+console.log(passTheClass);
+console.log(passWK4ClassLab);
