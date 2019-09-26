83df608fbb8105dd6a5cf2481239bafc29cbe44b
Lauren Kelly <lkelly785@gmail.com>
2019-09-25 21:31:45 -0400

    committing homework for week 4

--- week4/booleans/booleans.js
+++ week4/booleans/booleans.js
@@ -1,1 +1,14 @@
 
+let isMarried = "Married";
+let isAdult = "Adult";
+let isSenior = "Senior";
+let isDog = "Dog";
+let isDay = "Day";
+let isNight = "Night";
+
+console.log(isMarried);
+console.log(isAdult);
+console.log(isSenior);
+console.log(isDog);
+console.log(isDay);
+console.log(isNight);
--- week4/numbers/numbers.js
+++ week4/numbers/numbers.js
@@ -1,1 +1,14 @@
+let thingSailboat = 30000;
+let thingTripToMayanRuins = 2000;
+let thingNewCar = 20000;
+let thingCabinInVermont = 200000;
+let thingHouseOnMarthasVineyard = 600000;
+let thingTripToVietnam = 3000;
+let thingEthicalSarafriTrip = 5000;
+let thingHikingTripInAmazon = 5000;
+let thing6MoCanadianRoadtrip = 20000;
 
+let total = 885000
+
+
+console.log(total);
